32ae5c825e665e321a710c30904e4ad33570a7c9
Pedro <pedro.fontes@aluno.unifafibe.edu.br>
2022-08-13 11:18:13 -0300

    Adicionar arquivos de projeto.

---
+++ Aula1/Aula1/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Aula1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCadastrar_Clicked(object sender, EventArgs e)
+        {
+            
+            DisplayAlert("Atenção", "Mensagem: Cadastro efetuado, confirme os dados abaixos" + "\n" +txtNome.Text + "\n" + txtIdade.Text + "\n" + txtSexo.Text + "\n" + txtTelefone.Text,"OK","Cancelar");
+        }
+    }
+}
